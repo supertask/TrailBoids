4f337e9d36a6ca3aa1e770026817e4c187724a26
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2018-07-01 13:11:05 +0900

    Added a boid trail path.

--- Assets/BoidController.cs
+++ Assets/BoidController.cs
@@ -6,7 +6,6 @@ namespace TrailBoids
     public class BoidController : MonoBehaviour
     {
         #region Editable properties
-
         [SerializeField] int _spawnCount = 10;
         [SerializeField] float _spawnRadius = 4;
 
@@ -16,8 +15,14 @@ namespace TrailBoids
 
         [SerializeField] float _rotationSpeed = 4;
         [SerializeField] float _neighborDistance = 2;
-
         #endregion
+
+        Path path;
+        Vector3 leaderPosition;
+        Vector3 leaderDirection;
+        float animeRate;
+        //Vector3 currentDirection; // Vector3.up; //Vector3.forward;
+        //Vector3 currentPosition;
 
         #region Boid array
 
@@ -47,10 +52,23 @@ namespace TrailBoids
         // Reynolds' steering behavior
         void SteerBoid(Boid self)
         {
+            //velocityを変更
+            Vector3 v = this.leaderDirection * -9.0f; //this.path.GetCurrentVelocity();
+            ParticleSystem.VelocityOverLifetimeModule velocity = self.gameObject.GetComponent<ParticleSystem>().velocityOverLifetime;
+            velocity.x = v.x; velocity.y = v.y; velocity.z = v.z;
+            var particle = self.gameObject.GetComponent<ParticleSystem>();
+            /*
+            Debug.Log("particle.x: " + particle.velocityOverLifetime.xMultiplier);
+            Debug.Log("particle.y: " + particle.velocityOverLifetime.yMultiplier);
+            Debug.Log("particle.z: " + particle.velocityOverLifetime.zMultiplier);
+            */
+
             // Steering vectors
             var separation = Vector3.zero;
-            var alignment = transform.forward;
-            var cohesion = transform.position;
+            //var alignment = transform.up;
+            var alignment = this.leaderDirection;
+            //var cohesion = new Vector3(0,40,0); //transform.position;
+            var cohesion = this.leaderPosition;
 
             // Looks up nearby boids.
             var neighborCount = 0;
@@ -63,7 +81,7 @@ namespace TrailBoids
 
                 // Influence from this boid
                 separation += GetSeparationVector(self, neighbor);
-                alignment += neighbor.rotation * Vector3.forward;
+                alignment += neighbor.rotation * this.path.GetDirection();
                 cohesion += neighbor.position;
 
                 neighborCount++;
@@ -76,25 +94,46 @@ namespace TrailBoids
 
             // Calculate the target direction and convert to quaternion.
             var direction = separation + alignment * 0.667f + cohesion;
-            var rotation = Quaternion.FromToRotation(Vector3.forward, direction.normalized);
+            var rotation = Quaternion.FromToRotation(this.path.GetDirection(), direction.normalized);
 
             // Applys the rotation with interpolation.
             if (rotation != self.rotation)
             {
+                //deltaTime = フレーム間の秒数, 0.016秒あたり
+                //x = 負の無限~0の場合，0 < e(x) < 1(x=0)
+                //x = 正の無限の場合，e(x) > 1
                 var ip = Mathf.Exp(-_rotationSpeed * Time.deltaTime);
-                self.rotation = Quaternion.Slerp(rotation, self.rotation, ip);
+                //if (this._boids[0] == self) { Debug.Log("ip: " + ip); }
+
+                //_rotationSpeedがあがるとip=0.9くらいになり，self.rotationに近く
+                //_rotationSpeedが少し下がるとip=0.3くらいになり，rotationに近くなる=targetに近づきすぎる
+                self.rotation = Quaternion.Slerp(rotation, self.rotation, ip); //ipの分だけrotationからself.rotationへ近づく
             }
         }
 
         // Position updater
         void AdvanceBoid(Boid self)
         {
-            var noise = Mathf.PerlinNoise(Time.time * 0.5f, self.noiseOffset) * 2 - 1;
-            var velocity = _velocity * (1 + noise * _velocityVariance);
-            var forward = self.rotation * Vector3.forward;
-            self.position += (forward * velocity + _scroll) * Time.deltaTime;
-        }
+            //PerlinNoise（なだらかに変動するランダム）によって時間による速度のグラデーションを作り出す．
+            //x = Time.time * 0.5f -> 1s=0.5, 2s=1.0, 3s=1.5, 4s=2.0
+            //y = self.noiseOffset, each boid has different noiseOffset
+            var noise = Mathf.PerlinNoise(Time.time * 0.5f, self.noiseOffset) * 2 - 1; //-1.0 ~ 1.0
 
+            //noise * _velocityVarianceが分散値
+            //_velocityVariance = 0.6, noise = -1.0 ~ 1.0 → (1 + noise * _velocityVariance) = -1.6 ~ 1.6
+            //つまり，_velocity = 6だと, velocity=4.0-10.0くらい, 1が6を維持するためのもの
+            var velocity = _velocity * (1 + noise * _velocityVariance); //4.0-9.9
+
+            //velocity += this.Accelerate(self);
+
+            var forward = self.rotation * this.path.GetDirection();
+            //self.position += (forward * velocity + _scroll) * Time.deltaTime;
+            //self.position += (forward * velocity) * Time.deltaTime; //スクロール０
+            var deltaPosition = (forward * velocity) * Time.deltaTime;
+            //Debug.Log("rotation: " + self.rotation.eulerAngles);
+            //Debug.Log("deltaPostion: " + deltaPosition);
+            self.position += deltaPosition;
+        }
         #endregion
 
         #region Public methods
@@ -126,7 +165,10 @@ namespace TrailBoids
 
         void Start()
         {
-            _template = transform.GetChild(0).gameObject;
+            this.path = new Path();
+            this.animeRate = 0.0f;
+
+            _template = transform.GetChild(0).gameObject; //パーティクルをとってくる
             _template.SetActive(false);
 
             for (var i = 0; i < _spawnCount; i++) Spawn();
@@ -134,15 +176,29 @@ namespace TrailBoids
 
         void Update()
         {
+            if (animeRate > 1.0f) {
+                this.path.currentIndex = (this.path.currentIndex + 1) % path.GetCount();
+                this.animeRate = 0.0f;
+            }
+            Debug.Log("rate: " + this.animeRate);
+            this.leaderPosition = Vector3.Lerp(
+                this.path.GetPosition(), this.path.GetNextPosition(), this.animeRate
+            );
+            Debug.Log("position: " + this.leaderPosition);
+            this.leaderDirection = this.path.GetDirection();
+            GameObject.Find("LeaderNode").transform.position = this.leaderPosition;
+
             foreach (var boid in _boids) SteerBoid(boid);
             foreach (var boid in _boids) AdvanceBoid(boid);
+            //'foreach (var boid in _boids) PathFollowing(boid);
 
-            foreach (var boid in _boids)
-            {
+            foreach (var boid in _boids) {
                 var tr = boid.gameObject.transform;
                 tr.position = boid.position;
                 tr.rotation = boid.rotation;
             }
+
+            this.animeRate+=0.005f;
         }
 
         #endregion
